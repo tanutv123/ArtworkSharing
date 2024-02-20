451c2c4ef1e8e6d58667b9d59ff7b3c733db3ae9
tanutv123 <taitanvo2309@gmail.com>
2024-02-20 22:05:37 +0700

    Test branch

---
+++ BusinessObject/Entities/Connection.cs
@@ -0,0 +1,19 @@
+﻿namespace BusinessObject.Entities
+{
+	public class Connection
+	{
+        public Connection()
+        {
+            
+        }
+
+        public Connection(string connectionId, string emailAddress)
+        {
+            ConnectionId = connectionId;
+            EmailAddress = emailAddress;
+        }
+
+        public string ConnectionId { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
