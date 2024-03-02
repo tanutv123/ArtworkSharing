dcc22a60e566cb3b837947be0ef87437b8d43a36
vsnghi95 <vsnghi95@gmail.com>
2024-03-02 15:35:19 +0700

    fix Delete Artwork

---
+++ BusinessObject/DTOs/UpdateArtworkImageDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.DTOs
+{
+	public class UpdateArtworkImageDTO
+	{
+		public int Id {  get; set; }
+		public string ArtworkId { get; set; }
+		[Required]
+		public string Url { get; set; }
+		public bool isMain { get; set; }
+		[Required]
+		public string PublicId { get; set; }
+	}
+}
