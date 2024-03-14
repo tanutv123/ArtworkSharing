7fc01d72541344571fdab2bfee6f8dcb4787ebbb
nguyenphuongkiet <KietNPSE172931@fpt.edu.vn>
2024-03-14 21:26:15 +0700

    fix conflict

--- DataAccess/Helpers/AutoMapperProfile.cs
+++ DataAccess/Helpers/AutoMapperProfile.cs
@@ -15,7 +15,6 @@ namespace DataAccess.Helpers
             CreateMap<CommissionRequest, CommissionRequestDTO>().ReverseMap();
             CreateMap<Artwork, AddArtworkDTO>().ReverseMap();
             CreateMap<ArtworkImage, AddArtworkImageDTO>().ReverseMap();
-<<<<<<< HEAD
             CreateMap<ArtworkImage, UpdateArtworkImageDTO>().ReverseMap();
             CreateMap<AppUser, AppUserProfileDTO>()
                 .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.UserImage.Url))
@@ -34,22 +33,10 @@ namespace DataAccess.Helpers
             CreateMap<AppUser, AppUserDTO>()
                 .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.UserRoles.FirstOrDefault().Role.Name))
                 .ReverseMap();
-=======
-			CreateMap<ArtworkImage, UpdateArtworkImageDTO>().ReverseMap();
+
             CreateMap<Purchase, AddPurchaseDTO>().ReverseMap();
             CreateMap<Transaction, AddTransationDTO>().ReverseMap();
-            CreateMap<AppUser, AppUserProfileDTO>()
-				.ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.UserImage.Url))
-				.ReverseMap();
-			CreateMap<CommissionRequest, CommissionRequestHistoryDTO>()
-				.ForMember(dest => dest.ReceiverEmail, opt => opt.MapFrom(src => src.Receiver.Email))
-				.ForMember(dest => dest.SenderEmail, opt => opt.MapFrom(src => src.Sender.Email))
-				.ForMember(dest => dest.CommissionStatusDescription, opt => opt.MapFrom(src => src.CommissionStatus.Description))
-				.ReverseMap();
-			CreateMap<AppUser, AppUserDTO>()
-				.ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.UserRoles.FirstOrDefault().Role.Name))
-				.ReverseMap();
->>>>>>> 57f893dfcbe92d3560bc91f9270b90e570fc36a8
+           
             CreateMap<AppUser, UserDetailDTO>().ReverseMap();
         }
     }
