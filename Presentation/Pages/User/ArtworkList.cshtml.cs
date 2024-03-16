d8742b89e744f53172e68ceb4b55c186307db16a
tanutv123 <taitanvo2309@gmail.com>
2024-03-16 21:19:14 +0700

    Add authorization attribute for artwork list

--- Presentation/Pages/User/ArtworkList.cshtml.cs
+++ Presentation/Pages/User/ArtworkList.cshtml.cs
@@ -1,4 +1,5 @@
 using BusinessObject.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Presentation.Extensions;
@@ -6,6 +7,7 @@ using Repository;
 
 namespace Presentation.Pages.User;
 
+[Authorize]
 public class ArtworkList : PageModel
 {
     private readonly IPurchaseRepository _purchaseRepository;
